6b9ccb7f55b486e3c40d7cac035119f4b9fd9253
anhnn2401 <coolboy95.it@gmail.com>
2020-05-11 16:24:56 +0700

    fix header - menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,3 @@
-function myFunction() {
-  var btnmenu = document.getElementById("idlistmenu");
-  if (btnmenu.className === "list-menu") {
-    btnmenu.className += " responsive-menu";
-  }
-  else {
-    btnmenu.className = "list-menu";
-  }
-}
+$('.btn-open-menu').on('click', function(){
+  $('.list-menu').toggleClass('open')
+})
